f4c60fc5b1acf3ba212a446f4448e3dfdf97a7cd
smarthackerp <apuneet71@gmail.com>
2020-09-23 17:13:43 +0530

    Successfully

---
+++ src/main/java/com/capgemini/onlinemovieticketsystem/controller/Controller.java
@@ -0,0 +1,8 @@
+package com.capgemini.onlinemovieticketsystem.controller;
+
+public class Controller {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Master");
+    }
+}
